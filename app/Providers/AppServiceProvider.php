1faca57f56ada5608d6208b3036aebfcc533fdee
AlbertWebs <albertmuhatia@gmail.com>
2025-12-01 12:46:12 +0300

    Mailsend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Mail\MailManager;
-use App\Mail\Transports\MailerSendSmtpTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,18 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Fix SSL certificate verification for MailerSend SMTP when hosting intercepts connections
-        // This is a workaround for hosting providers that intercept SMTP traffic
-        $this->app->afterResolving(MailManager::class, function (MailManager $manager) {
-            $manager->extend('mailersend', function (array $config) {
-                return new MailerSendSmtpTransport(
-                    $config['host'] ?? 'smtp.mailersend.net',
-                    $config['port'] ?? 587,
-                    $config['encryption'] ?? 'tls',
-                    $config['username'] ?? null,
-                    $config['password'] ?? null
-                );
-            });
-        });
+        //
     }
 }
